d822e0be06d420b0d76edc4ed67df7149acc3e74
Xusk947 <xusk947@gmail.com>
2024-03-14 12:04:03 +0000

    Initial

---
+++ .vscode/tests/test.ts
@@ -0,0 +1,9 @@
+import { GeckoTerminalApi } from "../../src";
+
+var api = new GeckoTerminalApi();
+
+async function test() {
+    
+}
+
+test()
---
+++ src/model/tradeResponse.ts
@@ -0,0 +1,27 @@
+
+export interface TradeAttributes {
+    block_number: number;
+    block_timestamp: string;
+    tx_hash: string;
+    tx_from_address: string;
+    from_token_amount: string;
+    to_token_amount: string;
+    price_from_in_currency_token: string;
+    price_to_in_currency_token: string;
+    price_from_in_usd: string;
+    price_to_in_usd: string;
+    kind: string;
+    volume_in_usd: string;
+    from_token_address: string;
+    to_token_address: string;
+}
+
+export interface Trade {
+    id: string;
+    type: string;
+    attributes: TradeAttributes;
+}
+
+export interface TradesResponse {
+    data: Trade[];
+}
